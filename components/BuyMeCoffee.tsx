b0d5da77db13b56d0d16f52504956d9f0b6c7406
ARYPROGRAMMER <arya.2023ug1104@iiitranchi.ac.in>
2024-12-13 22:10:07 +0530

    fixtures: bmc widget

--- components/BuyMeCoffee.tsx
+++ components/BuyMeCoffee.tsx
@@ -1,21 +1,24 @@
-import Script from "next/script";
-
 export const BuyMeCoffeeButton = () => {
   return (
     <div>
-      <Script
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        data-id="aryasingh"
-        data-description="Support me building CodeX"
-        data-message="Help Me Build More Open Source Projects"
-        data-color="#40DCA5"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"
-        strategy="lazyOnload" // Ensures the script loads without blocking
-      />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+            <script
+              data-name="BMC-Widget"
+              data-cfasync="false"
+              src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+              data-id="aryasingh"
+              data-description="Support me building CodeX"
+              data-message="Help Me Build More Open Source Projects"
+              data-color="#40DCA5"
+              data-position="Right"
+              data-x_margin="18"
+              data-y_margin="18">
+            </script>
+          `,
+        }}
+      ></div>
     </div>
   );
 };
